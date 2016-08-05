ba72e3ab2583438c0d3d3082866219079c7b7b97
Lars Kastner <lars.kastner@yahoo.dk>
2016-08-05 16:32:51 -0400

    CHOSEN_BASES introduced, BLOCKS rewritten

---
+++ scripts/examplePC.cmd
@@ -0,0 +1,21 @@
+application "fan";
+$f = new PolyhedralFan(RAYS=>[[1,0,0],[1,1,0],[1,0,1],[1,1,1]], MAXIMAL_CONES=>[[0,1,2],[1,2,3]]);
+$pc = new PolyhedralComplex($f);
+print keys %{$pc->CHOSEN_BASES};
+
+$s = new Set<Int>(1,2,3);
+print $pc->CHOSEN_BASES->{$s};
+
+$pc->BLOCKS;
+$h = new Set<Int>([0]);
+print $pc->CHOSEN_BASES->{$h}->type->full_name;
+
+
+$t = new Set<Int>(2,3);
+$pst = new Pair<Set<Int>, Set<Int> >($s, $t);
+print $pc->BLOCKS->{$pst};
+print $pc->CHOSEN_BASES->{$s};
+print $pc->CHOSEN_BASES->{$t};
+
+
+
