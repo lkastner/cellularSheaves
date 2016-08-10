4c4aa559cd92fa16797ac10d580a41cb53c1d221
Anna-Lena Winz <annalena.winz@gmail.com>
2016-08-10 17:19:23 +0200

    Workaround for zero_matrix. Reduced debug-output

--- apps/fan/include/linalg_tools.h
+++ apps/fan/include/linalg_tools.h
@@ -35,11 +35,12 @@ namespace polymake { namespace fan{
 
 template <typename E> inline
 Matrix<E>
-wedge_matrix(const Matrix<E>& input, int n){
+wedge_matrix_cpp(const Matrix<E>& input, int n){
    int nrows, ncols, resultNrows, resultNcols, i, j;
    Matrix<E> result;
    nrows = input.rows();
    ncols = input.cols();
+   // cout << "choose: " << ncols << " " << n<<endl;
    resultNrows = Integer::binom(nrows, n).to_int();
    resultNcols = Integer::binom(ncols, n).to_int();
    if((resultNrows == 0) || (resultNcols == 0)){
@@ -49,7 +50,7 @@ wedge_matrix(const Matrix<E>& input, int n){
    i=0;
    for (Entire<Subsets_of_k<const sequence&> >::const_iterator selectedRow=entire(all_subsets_of_k(sequence(0,nrows),n)); !selectedRow.at_end(); ++selectedRow) {
       j=0;
-      cout << *selectedRow << endl;
+      // cout << *selectedRow << endl;
       for (Entire<Subsets_of_k<const sequence&> >::const_iterator selectedCol=entire(all_subsets_of_k(sequence(0,ncols),n)); !selectedCol.at_end(); ++selectedCol) {
          result(i,j) = pm::det(input.minor(*selectedRow, *selectedCol));
          j++;
@@ -61,7 +62,7 @@ wedge_matrix(const Matrix<E>& input, int n){
 
 template <typename E> inline
 Matrix<E>
-choose_basis(const Matrix<E>& input){
+choose_basis_cpp(const Matrix<E>& input){
 	int desired = rank(input), current = 0, test;
 	Matrix<E> result(0, input.cols());
 	for(typename Entire<Rows<Matrix<E> > >::const_iterator rowit = entire(rows(input)); !rowit.at_end(); ++rowit){
@@ -79,30 +80,33 @@ choose_basis(const Matrix<E>& input){
 
 template <typename E> inline
 Matrix<E>
-assemble_matrix(const Array<Set<int> > sigmas, 
+assemble_matrix_cpp(const Array<Set<int> > sigmas, 
                const Array<Set<int> > taus, 
                const Map<std::pair<Set<int>, Set<int> >, Matrix<E> > blocks, 
                const Map<std::pair<Set<int>, Set<int> >, int> orientations){
    int nrows = taus.size(), ncols = sigmas.size(), i, j, blockRow, blockCol;
    std::pair<Set<int>, Set<int> > first_pair(sigmas[0], taus[0]), currentPair;
-   cout << "FP: " << first_pair << endl;
-   cout << blocks << endl;
+   //cout << "FP: " << first_pair << endl;
+   //cout << blocks << endl;
    blockRow = blocks[first_pair].rows();
    blockCol = blocks[first_pair].cols();
    nrows *= blockRow;
    ncols *= blockCol;
+   if((nrows == 0) || (ncols == 0)){
+      return zero_matrix<E>(nrows, ncols);
+   }
    Matrix<E> result(nrows, ncols);
    i = 0;
    for(typename Entire<Array<Set<int> > >::const_iterator tau = entire(taus); !tau.at_end(); ++tau){
       j = 0;
       for(typename Entire<Array<Set<int> > >::const_iterator sigma = entire(sigmas); !sigma.at_end(); ++sigma){
          currentPair = std::pair<Set<int>, Set<int> >(*sigma, *tau);
-         cout << "Hello." << endl;
-         cout << result << endl;
-         cout << blocks[currentPair] << endl;
-         cout << i*blockRow << " " << (i+1)*blockRow << endl;
-         cout << j*blockCol << " " << (j+1)*blockCol << endl;
-         cout << sequence(3,5) << endl;
+         // cout << "Hello." << endl;
+         // cout << result << endl;
+         // cout << blocks[currentPair] << endl;
+         // cout << i*blockRow << " " << (i+1)*blockRow << endl;
+         // cout << j*blockCol << " " << (j+1)*blockCol << endl;
+         // cout << sequence(3,5) << endl;
          result.minor(sequence(i*blockRow, blockRow), sequence(j*blockCol, blockCol)) = orientations[currentPair] * blocks[currentPair];
          j++;
       }
