fea4690c4650ff0d578ba76a3e16eddf2ed4044f
Kristin Shaw <krrshaw@gmail.com>
2016-08-10 18:30:49 -0400

    finished up fcomplex, spits out zero maps though. line 9 needs to be fixed

--- scripts/kristin.cmd
+++ scripts/kristin.cmd
@@ -2,9 +2,14 @@ application "fan";
 $f = new PolyhedralFan(RAYS=>[[1,0,0],[1,1,0],[1,0,1],[1,1,1]], MAXIMAL_CONES=>[[0,1,2],[1,2,3]]);
 $pc = new PolyhedralComplex($f);
 $tau = $pc->HASSE_DIAGRAM->FACES->[3];
+$sigma = $pc->HASSE_DIAGRAM->FACES->[2];
 print find_max_containing($tau, $pc);
 print build_matrix_f($tau, $pc, 1);
+print build_blocks_f($pc, 1);
 
+$key = new Pair<Set<Int>, Set<Int> >($sigma, $tau);
+print $key;
+print build_blocks_f($pc, 1)->{$key};
 
 
 $v = new Vector("0 0 0 0 0 1");
