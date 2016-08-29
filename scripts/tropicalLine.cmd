ba960e4694f345a6e62accec0bb9ff873d4fa073
Lars Kastner <lars.kastner@yahoo.dk>
2016-08-29 18:14:56 -0400

    Tropical line

---
+++ scripts/tropicalLine.cmd
@@ -0,0 +1,11 @@
+
+###########################################################
+application "fan";
+$pc = new PolyhedralComplex(VERTICES=>[[1,0,0],[0,-1,0],[0,0,-1],[0,1,1]], MAXIMAL_POLYTOPES=>[[0,1],[0,2],[0,3]]);
+print $pc->SIMPLE_BLOCKS;
+$w = $pc->wcomplex(1);
+print $w->CHAIN_COMPLEX->DIFFERENTIALS;
+print $w->CHAIN_COMPLEX->BETTI_NUMBERS;
+$f1 = $pc->fcomplex(1);
+$boundedChain = build_chain_complex($f1->BLOCKS, $pc->BOUNDED_FACES, $pc->ORIENTATIONS);
+
