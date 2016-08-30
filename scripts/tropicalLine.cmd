8f6f31dc9f1f8760a88975f3c1a997081e656558
Lars Kastner <lars.kastner@yahoo.dk>
2016-08-30 11:33:43 -0400

    Fixed wcomplex? Should sheaves be cosheaves?

--- scripts/tropicalLine.cmd
+++ scripts/tropicalLine.cmd
@@ -4,6 +4,7 @@ application "fan";
 $pc = new PolyhedralComplex(VERTICES=>[[1,0,0],[0,-1,0],[0,0,-1],[0,1,1]], MAXIMAL_POLYTOPES=>[[0,1],[0,2],[0,3]]);
 print $pc->SIMPLE_BLOCKS;
 $w = $pc->wcomplex(1);
+$w->CHAIN_COMPLEX->print();
 print $w->CHAIN_COMPLEX->DIFFERENTIALS;
 print $w->CHAIN_COMPLEX->BETTI_NUMBERS;
 $f1 = $pc->fcomplex(1);
