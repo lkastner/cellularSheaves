befdb3541e3ebac7a8101bef77ec77321ade330e
Lars Kastner <lars.kastner@yahoo.dk>
2016-08-31 14:02:10 -0400

    Halfway fixed

--- scripts/lars.cmd
+++ scripts/lars.cmd
@@ -129,7 +129,7 @@ print $bm1->IS_WELLDEFINED;
 ###############################################################################
 
 application "tropical";
-$f = toTropicalPolynomial("max(0,x,y, x+y+1)");
+$f = toTropicalPolynomial("max(0,x+5,y+3, x+y+9)");
 $div = tropical::divisor( (projective_torus<Max>(2)) , rational_fct_from_affine_numerator($f));
 application "fan";
 print $div->ORIENTATIONS;
