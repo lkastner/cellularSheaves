1f4b24d65f872446d4b4e7e2569c664289c70dcb
kristin shaw <krrshaw@gmail.com>
2016-09-27 12:26:47 -0400

    added to demo.cmd

--- scripts/demo.cmd
+++ scripts/demo.cmd
@@ -3,12 +3,34 @@
 
 application "fan";
 $pc = new PolyhedralComplex(check_fan_objects(new Cone(cube(3))));
+print $pc->HASSE_DIAGRAM->FACES;
+print $pc->ORIENTATIONS;
+$w1 = $pc->wsheaf(1);
+print $w1->BASES;
+print $w1->BLOCKS;
+print $w1->CHAIN_COMPLEX->BETTI_NUMBERS;
+print $w1->CHAIN_COMPLEX->HOMOLOGIES;
+
+$w2 = $pc->wsheaf(2);
+print $w2->BASES;
+print $w2->BLOCKS;
+print $w2->CHAIN_COMPLEX->BETTI_NUMBERS;
+
+$w3 = $pc->wsheaf(3);
+print $w3->CHAIN_COMPLEX->BETTI_NUMBERS;
+
+$w0 = $pc->wsheaf(0);
+print $w0->CHAIN_COMPLEX->BETTI_NUMBERS;
+
 @result = ();
 for(my $i=0; $i<4; $i++){
    my $w = $pc->wsheaf($i);
    push @result, $w->CHAIN_COMPLEX->BETTI_NUMBERS;
 }
 print new Matrix(@result);
+
+$cube = polytope::cube(3);
+print $cube->DUAL_H_VECTOR;
 
 $pc = new PolyhedralComplex(check_fan_objects(new Cone(simplex(5))));
 @result = ();
@@ -18,16 +40,59 @@ for(my $i=0; $i<6; $i++){
 }
 print new Matrix(@result);
 
+$simplex = polytope::simplex(5);
+print $simplex->DUAL_H_VECTOR;
 
-# Tropical linear spaces
+
+# Bergman fans and tropical linear spaces
+
+application "graph";
+$g = complete(4);
+application "matroid";
+$m = matroid_from_graph($g);
+print $m->BASES;
+application "tropical";
+$t = matroid_fan<Max>($m);
+application "fan";
+print $t->RAYS;
+$berg = new PolyhedralComplex($t);
+$f0 = $berg->fcosheaf(0);
+$f1 = $berg->fcosheaf(1);
+$f2 = $berg->fcosheaf(2);
+$f3 = $berg->fcosheaf(3);
+$s0 = $berg->usual_chain_complex($f0);
+$s1 = $berg->usual_chain_complex($f1);
+$s2 = $berg->usual_chain_complex($f2);
+$s3 = $berg->usual_chain_complex($f3);
+print $s0->BETTI_NUMBERS;
+print $s1->BETTI_NUMBERS;
+print $s2->BETTI_NUMBERS;
+print $s3->BETTI_NUMBERS;
+
+$bm0 = $berg->borel_moore_complex($f0);
+$bm1 = $berg->borel_moore_complex($f1);
+$bm2 = $berg->borel_moore_complex($f2);
+$bm3 = $berg->borel_moore_complex($f3);
+print $bm0->BETTI_NUMBERS;
+print $bm1->BETTI_NUMBERS;
+print $bm2->BETTI_NUMBERS;
+print $bm3->BETTI_NUMBERS;
+
 
 $v = [0,0,3,1,2,1,0,1,0,2,2,0,3,0,4,1,2,2,0,0];
 $val_matroid = new matroid::ValuatedMatroid<Min>(BASES=>matroid::uniform_matroid(3,6)->BASES,VALUATION_ON_BASES=>$v,N_ELEMENTS=>6);
 $tls = tropical::linear_space($val_matroid);
 @result = ();
 for(my $i=0;$i<3;$i++){
-   my $wi = $tls->wsheaf($i);
-   my $si=$tls->usual_chain_complex($wi);
+   my $fi = $tls->fsheaf($i);
+   my $si=$tls->usual_chain_complex($fi);
+   push @result, $si->BETTI_NUMBERS;
+}  
+print new Matrix(@result);
+
+for(my $i=0;$i<3;$i++){
+   my $fi = $tls->fsheaf($i);
+   my $si=$tls->borel_moore_complex($fi);
    push @result, $si->BETTI_NUMBERS;
 }  
 print new Matrix(@result);
