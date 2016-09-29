ce2f957d80b6f1c600934cf1803aa1e97abb7464
Anna-Lena Winz <annalena.winz@googlemail.com>
2016-09-29 12:12:44 -0400

    all code in demo working (on my machine), and added some printouts of complexes

--- scripts/demo.cmd
+++ scripts/demo.cmd
@@ -59,12 +59,18 @@ $f2 = $berg->fcosheaf(2);
 $s0 = $berg->usual_chain_complex($f0);
 $s1 = $berg->usual_chain_complex($f1);
 $s2 = $berg->usual_chain_complex($f2);
+$s0->print();
+$s1->print();
+$s2->print();
 print $s0->BETTI_NUMBERS;
 print $s1->BETTI_NUMBERS;
 print $s2->BETTI_NUMBERS;
 $bm0 = $berg->borel_moore_complex($f0);
 $bm1 = $berg->borel_moore_complex($f1);
 $bm2 = $berg->borel_moore_complex($f2);
+$bm0->print();
+$bm1->print();
+$bm2->print();
 print $bm0->BETTI_NUMBERS;
 print $bm1->BETTI_NUMBERS;
 print $bm2->BETTI_NUMBERS;
@@ -155,11 +161,11 @@ print new Matrix(@result2);
 
 
 
-
+application "tropical";
 $g = toTropicalPolynomial("max(0,x,y, 2*x - 2, 2*y-2, x+y-1, 3*x-6, 3*y-6, 2*x+y - 4,2*y + x- 4, 4*x -12, 4*y-12,  3*x+y -9,3*y + x- 9, 2*x + 2*y - 8)");
 
-$gen3 = tropical::divisor( (projective_torus<Max>(2)) , rational_fct_from_affine_numerator)($f));  
-
+$gen3 = tropical::divisor( (projective_torus<Max>(2)) , rational_fct_from_affine_numerator($g));  
+application "fan";
 $f0 = $gen3->fcosheaf(0);
 $f1 = $gen3->fcosheaf(1);
 
@@ -167,22 +173,28 @@ $us0 = $gen3->usual_chain_complex($f0);
 $us1 = $gen3->usual_chain_complex($f1);
 
 $bm0 = $gen3->borel_moore_complex($f0);
-$bm1 = $gen3->borel_moore_compelx($f1);
+$bm1 = $gen3->borel_moore_complex($f1);
+
+$us0->print();
+$us1->print();
 
 print $us0->BETTI_NUMBERS;
 print $us1->BETTI_NUMBERS;
+
+$bm0->print();
+$bm1->print();
 
 print $bm0->BETTI_NUMBERS;
 print $bm1->BETTI_NUMBERS;
 
 
 
-
+application "tropical";
 $f = toTropicalPolynomial("max(0,x,y,z, 2*x - 2, 2*y-2, 2*z-2, x+y-1, x+z-1, y+z-1, 3*x-6, 3*y-6, 3*z-6, 2*x+y - 4,2*y + x- 4, 2*x + z - 4, 2*z+x - 4, 2*y + z- 4, 2*z+y - 4, x+y+z +1, 4*x -12, 4*y-12, 4*z-12,  3*x+y -9,3*y + x- 9, 3*x + z - 9, 3*z+x - 9, 3*y + z- 9, 3*z+y - 9, 2*x + 2*y - 8, 2*x + 2*z - 8, 2*y + 2*z - 8,2*x + y +z - 7, x + 2*z +y - 7, 2*y + z + x- 7  )");
 
-$k3 = tropical::divisor( (projective_torus<Max>(3)) , rational_fct_from_affine_numerator(
-$f));
+$k3 = tropical::divisor( (projective_torus<Max>(3)) , rational_fct_from_affine_numerator($f));
 
+application "fan";
 print $k3->BOUNDED_FACES->{0}->size;
 
 $f0 = $k3->fcosheaf(0);
@@ -197,16 +209,17 @@ $bm0 = $k3->borel_moore_complex($f0);
 $bm1 = $k3->borel_moore_complex($f1);
 $bm2 = $k3->borel_moore_complex($f2);
 
+$us0->print();
 print $us0->BETTI_NUMBERS;
+$us1->print();
 print $us1->BETTI_NUMBERS;
+$us2->print();
 print $us2->BETTI_NUMBERS;
 
+$bm0->print();
 print $bm0->BETTI_NUMBERS;
+$bm1->print();
 print $bm1->BETTI_NUMBERS;
+$bm2->print();
 print $bm2->BETTI_NUMBERS;
-
-
-
-
-
 
