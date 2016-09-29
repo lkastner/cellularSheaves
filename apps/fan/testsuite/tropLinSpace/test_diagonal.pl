2ea94e76ebf9dcc1eaa0a0aa068b0d6469bf2496
Lars Kastner <lars.kastner@yahoo.dk>
2016-09-29 17:53:07 -0400

    Improving names.

--- apps/fan/testsuite/tropLinSpace/test_diagonal.pl
+++ apps/fan/testsuite/tropLinSpace/test_diagonal.pl
@@ -24,5 +24,11 @@ sub test_tls{
 }
 
 ## build the tropical space in polymake and save it.
-my $tls = load("00100010000000101000.poly");
-test_tls($tls);
+my @tlss = ("00100010000000101000.poly",  "10200100001110200100.poly",  
+"00312101021020313100.poly",  "30200202122320400301.poly",  
+"00312101022030412200.poly",  "40400403334440400403.poly",
+"10100200001110200100.poly");
+for my $tls (@tlss){
+   my $tlsReal = load($tls);
+   test_tls($tlsReal);
+}
