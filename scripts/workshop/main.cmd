74fd366a82a06de255a05f9a83965a9a2d0c0eec
Lars Kastner <lars.kastner@yahoo.dk>
2017-02-03 10:10:52 +0100

    Fixed conflict

--- scripts/workshop/main.cmd
+++ scripts/workshop/main.cmd
@@ -3,17 +3,23 @@ application "fan";
 $pc = new PolyhedralComplex(check_fan_objects(new Cone(cube(3))));
 print $pc->HASSE_DIAGRAM->FACES;
 $w1 = $pc->wsheaf(1);
+; #Building a sheaf from bases:
+print $w1->BASES->type->full_name;
 print $w1->BASES->{new Set<Int>([0,1,2,3,4,5,6,7])};
 print $w1->BASES->{new Set<Int>([0,1,4,5])};
 print $w1->BASES->{new Set<Int>([0,2])};
+print $w1->BLOCKS->type->full_name;
 print $w1->BLOCKS->{new Set<Set<Int>>([[0,1,2,3,4,5,6,7], [0,1,4,5]])};
 print $w1->BLOCKS->{new Set<Set<Int>>([[0,1,4,5], [0,2]])};
 ; # Building the chain complex
+print $pc->ORIENTATIONS->type->full_name;
 print $pc->ORIENTATIONS->{new Set<Set<Int>>([[0,1,2,3,4,5,6,7], [0,1,4,5]])};
 print $pc->ORIENTATIONS->{new Set<Set<Int>>([[0,1,4,5], [0,2]])};
 $cs1 = $pc->compact_support_complex($w1);
 $cs1->print();
+print $cs1->BETTI_NUMBERS->type->full_name;
 print $cs1->BETTI_NUMBERS;
+print $cs1->COHOMOLOGIES->type->full_name;
 print $cs1->COHOMOLOGIES;
 
 ; # Betti vs h-vector
@@ -63,7 +69,7 @@ print new Matrix(@betti_bm);
 
 ; # Orlik-Solomon algebra of a valuated matroid
 $v = [0,0,3,1,2,1,0,1,0,2,2,0,3,0,4,1,2,2,0,0];
-$val_matroid = new matroid::ValuatedMatroid<Min>( 
+$val_matroid = new matroid::ValuatedMatroid<Min>(
    BASES=>matroid::uniform_matroid(3,6)->BASES, 
    VALUATION_ON_BASES=>$v,N_ELEMENTS=>6);
 $tls = tropical::linear_space($val_matroid);
