270333952d7495ec5f66aaf2611d052ad5a7164b
Lars Kastner <lars.kastner@yahoo.dk>
2017-03-20 17:56:50 +0100

    Whatever

--- scripts/lars.cmd
+++ scripts/lars.cmd
@@ -221,3 +221,29 @@ $hd = $pc->HASSE_DIAGRAM;
 $R = realisation($A, $O);
 print old_closure($R, $hd);
 print is_face($A,$O,$hd);
+
+
+application "fan";
+$m = matroid::uniform_matroid(3,4);
+$t = tropical::matroid_fan<Max>($m);
+print $t->COMPACTIFICATION->ALL_FACES;
+print $t->COMPACTIFICATION->VERTICES;
+
+application "graph";
+$g = complete(4);
+application "matroid";
+$m = matroid_from_graph($g);
+application "tropical";
+$t = matroid_fan<Max>($m);
+$t->VERTICES;
+application "fan";
+$berg = new PolyhedralComplex($t);
+
+
+application "tropical";
+$f = toTropicalPolynomial("max(0,x+5,y+3, x+y+9)");
+$div = divisor( (projective_torus<Max>(2)), rational_fct_from_affine_numerator($f));
+application "fan";
+
+
+
