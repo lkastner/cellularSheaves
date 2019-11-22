bcfb7657d491cc6bf647ae8f9a276275400986d5
Lars Kastner <lars.kastner@yahoo.dk>
2019-11-22 14:51:34 +0100

    Some tests for fcosheaves on compactification

---
+++ apps/fan/testsuite/hodge/test.pl
@@ -0,0 +1,37 @@
+my $k3 = load("k3.pcom");
+my @rows_k3;
+for(my $i=0; $i<3; $i++){
+   my $f = $k3->compact_fcosheaf($i);
+   my $d = build_full_chain($k3->COMPACTIFICATION, $k3->COMPACTIFICATION->ORIENTATIONS, $f, false);
+   my $c = new ChainComplex(INPUT_DIFFERENTIALS=>$d);
+   push @rows_k3, $c->BETTI_NUMBERS;
+}
+my $computed = new Matrix(@rows_k3);
+my $desired = new Matrix([[1,0,1],[0,20,0],[1,0,1]]);
+compare_values("k3", $computed, $desired);
+
+
+my $cube2 = new PolyhedralComplex(check_fan_objects(cube(2)));
+my @rows_cube2;
+for(my $i=0; $i<3; $i++){
+   my $f = $cube2->compact_fcosheaf($i);
+   my $d = build_full_chain($cube2->COMPACTIFICATION, $cube2->COMPACTIFICATION->ORIENTATIONS, $f, false);
+   my $c = new ChainComplex(INPUT_DIFFERENTIALS=>$d);
+   push @rows_cube2, $c->BETTI_NUMBERS;
+}
+$computed = new Matrix(@rows_cube2);
+$desired = new Matrix([[1,0,0],[2,0,0],[1,0,0]]);
+compare_values("cube2", $computed, $desired);
+
+my $cube3 = new PolyhedralComplex(check_fan_objects(cube(3)));
+my @rows_cube3;
+for(my $i=0; $i<4; $i++){
+   my $f = $cube3->compact_fcosheaf($i);
+   my $d = build_full_chain($cube3->COMPACTIFICATION, $cube3->COMPACTIFICATION->ORIENTATIONS, $f, false);
+   my $c = new ChainComplex(INPUT_DIFFERENTIALS=>$d);
+   push @rows_cube3, $c->BETTI_NUMBERS;
+}
+$computed = new Matrix(@rows_cube3);
+$desired = new Matrix([[1,0,0,0],[3,0,0,0],[3,0,0,0],[1,0,0,0]]);
+compare_values("cube3", $computed, $desired);
+
