841b6711cd28a387b2a8da5b9ed80a156acec09c
Lars Kastner <lars.kastner@yahoo.dk>
2019-11-25 14:57:56 +0100

    Fix script for Hodge diamond of K3

--- scripts/compact.pl
+++ scripts/compact.pl
@@ -12,7 +12,6 @@ for(my $i=0; $i<3; $i++){
    $Polymake::User::Verbose::cpp = 3;
    my $d1 = build_full_chain($k3->COMPACTIFICATION, $k3->COMPACTIFICATION->ORIENTATIONS, $f1, false);
    # print $d1;
-   my $c1 = new ChainComplex(INPUT_DIFFERENTIALS=>$d1);
-   print $c1->BETTI_NUMBERS,"\n";
+   print topaz::betti_numbers($d1),"\n";
 }
 # print $k3->COMPACTIFICATION->DECORATION;
