b3bd97c13f24d19477847a1218cd4d587192c04a
Lars Kastner <lars.kastner@yahoo.dk>
2019-12-11 11:23:04 +0100

    Remove further workarounds, since bug in polymake got fixed

--- apps/fan/testsuite/assemble_complex/test.pl
+++ apps/fan/testsuite/assemble_complex/test.pl
@@ -17,7 +17,7 @@ for(my $i=0;$i<3;$i++){
 my $k3 = load("k3.pcom");
 for(my $i=0; $i<3; $i++){
    my $f = $k3->compact_fcosheaf($i);
-   my $d = new topaz::ChainComplex<Matrix<Rational>>(build_full_chain($k3->COMPACTIFICATION, $k3->COMPACTIFICATION->ORIENTATIONS, $f, false));
+   my $d = build_full_chain($k3->COMPACTIFICATION, $k3->COMPACTIFICATION->ORIENTATIONS, $f, false);
    compare_data("k3_$i", $d);
 }
 
