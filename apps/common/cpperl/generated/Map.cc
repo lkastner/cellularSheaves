67aaa6abd0e0f6ac00b9f924fd7907a333c778e1
Lars Kastner <lars.kastner@yahoo.dk>
2020-01-23 11:32:12 +0100

    Wrappers

---
+++ apps/common/cpperl/generated/Map.cc
@@ -0,0 +1,15 @@
+// This is an automatically generated file, any manual changes done here will be overwritten!
+// To change the generated C++ code, edit the script support/generate_cpperl_modules.pl
+// To change the number of instances per source file, specify "chunksize":N in ../Map.cpperl
+
+#define POLYMAKE_CPPERL_FILE "Map"
+#include "polymake/IncidenceMatrix.h"
+#include "polymake/Map.h"
+#include "polymake/Matrix.h"
+#include "polymake/Rational.h"
+#include "polymake/Set.h"
+#include "polymake/client.h"
+
+namespace polymake { namespace common { namespace {
+Class4perl(0, Polymake::common::Map_A_Set__Int_I_Pair_A_Matrix_A_Rational_I_NonSymmetric_Z_I_Matrix_A_Rational_I_NonSymmetric_Z_Z_Z, Map<Set<Int>, std::pair<Matrix<Rational>, Matrix<Rational>>>);
+} } }
