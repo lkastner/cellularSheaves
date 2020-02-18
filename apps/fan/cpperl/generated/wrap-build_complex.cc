e22d77fb1b6ad97873b2f1497e86564f52ec84a7
Lars Kastner <lars.kastner@yahoo.dk>
2020-02-18 11:51:29 +0100

    Wrapper

--- apps/fan/cpperl/generated/wrap-build_complex.cc
+++ apps/fan/cpperl/generated/wrap-build_complex.cc
@@ -4,6 +4,7 @@
 #define POLYMAKE_CPPERL_FILE "wrap-build_complex"
 #include "polymake/perl/macros.h"
 #include FindDefinitionSource4perl(build_complex.cc)
+#include "polymake/GF2.h"
 #include "polymake/Graph.h"
 #include "polymake/IncidenceMatrix.h"
 #include "polymake/Matrix.h"
@@ -17,4 +18,5 @@ enum class build_full_chain;
 };
 FunctionCaller4perl(build_full_chain, free_t);
 FunctionTemplateInstance4perl(0, build_full_chain, free_t, build_full_chain:T3.B.X.X.x, perl::Returns::normal, 3, (fan::compactification::SedentarityDecoration, graph::lattice::Nonsequential, Rational, void, perl::Canned<const EdgeMap<Directed, Int>&>, perl::Canned<const EdgeMap<Directed, Matrix<Rational>>&>, void));
+FunctionTemplateInstance4perl(1, build_full_chain, free_t, build_full_chain:T3.B.X.X.x, perl::Returns::normal, 3, (fan::compactification::SedentarityDecoration, graph::lattice::Nonsequential, GF2, void, perl::Canned<const EdgeMap<Directed, Int>&>, perl::Canned<const EdgeMap<Directed, Matrix<GF2>>&>, void));
 } } }
